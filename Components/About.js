9d35ef00fcbb4fa9a331eb09b9e1cb2c62f7dd06
Kourtney Smith <klsmith5370@gmail.com>
2023-03-24 21:16:18 -0400

    Updated about component with skills

--- Components/About.js
+++ Components/About.js
@@ -3,14 +3,17 @@ import React from "react"
 export default function About() {
     return (
         <div className="about-info">
-            <h2>About</h2>
-            <p>For the last 6 years, I was working for customer service positions for e-commerce companies and while these roles did provide me with a lot of personable and collaborative skills, I wanted to take on a new career path and I found myself looking into software engineering. 
-
-            After taking a course through my local community college on Python and a prep course in JavaScript, I soon gained an interest and I started looking into different programs that could take me to the next level. Upon recommendation from my friend, I applied for the Grace Hopper Web Development bootcamp. While in the bootcamp, we were taught about back-end development, with Express and Sequelize, and front-end development, with React and React-Redux, to gain an understanding in full stack development.
-            
-
-            After graduating from the bootcamp, I have learned a lot more about JavaScript, React, Redux, Express, and Sequelize while I went through the bootcamp and I am now looking forward to taking all of my newfound skills and start my engineering career.
-            </p>
+            <h2>Skills:</h2>
+            <ul>
+                <h3>Frontend:</h3>
+                <ul>JavaScript</ul>
+                <ul>React</ul>
+                <ul>Redux</ul>
+                <h3>Backend:</h3>
+                <ul>Express</ul>
+                <ul>Sequelize</ul>
+                <ul>postgreSQL</ul>
+            </ul>
         </div>
     )
 }
